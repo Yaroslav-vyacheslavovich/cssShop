bc4bf89863511e8a97589aca050f5b36195aa25a
Yaroslav <yarost12@yandex.ru>
2021-04-09 07:17:59 +0300

    add hw2

---
+++ js/script.js
@@ -0,0 +1,12 @@
+
+
+function openMenu() {
+    document.querySelector(".header__navigation").style.visibility = "visible";
+}
+function closeMenu() {
+    document.querySelector(".header__navigation").style.visibility = "hidden";
+}
+
+
+
+
